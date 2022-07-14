a27eba8a62b3e8b36578d2e56056c968ed8c33a3
Bruno Sajermann <sajermannbruno@gmail.com>
2022-07-14 19:35:03 -0300

    test

--- src/Email/index.test.ts
+++ src/Email/index.test.ts
@@ -1,38 +1,38 @@
-// import { isValidEmail } from '.';
+import { isValidEmail } from '.';
 
-// describe('Utils/isValidEmail', () => {
-// 	const emailWrong1 = 'test.com';
-// 	const emailWrong2 = 'test@.';
-// 	const emailWrong3 = 'test.com@test.';
-// 	const emailCorrectly1 = 'test@gmail.com';
-// 	const emailCorrectly2 = 'test@ab-inbev.com';
-// 	const emailCorrectly3 = 'test-ext@ab-inbev.com';
+describe('Utils/isValidEmail', () => {
+	const emailWrong1 = 'test.com';
+	const emailWrong2 = 'test@.';
+	const emailWrong3 = 'test.com@test.';
+	const emailCorrectly1 = 'test@gmail.com';
+	const emailCorrectly2 = 'test@ab-inbev.com';
+	const emailCorrectly3 = 'test-ext@ab-inbev.com';
 
-// 	it(`Must return Falsy`, () => {
-// 		expect(isValidEmail(emailWrong1)).toBeFalsy();
-// 	});
+	it(`Must return Falsy`, () => {
+		expect(isValidEmail(emailWrong1)).toBeFalsy();
+	});
 
-// 	it(`Must return Falsy`, () => {
-// 		expect(isValidEmail(emailWrong2)).toBeFalsy();
-// 	});
+	it(`Must return Falsy`, () => {
+		expect(isValidEmail(emailWrong2)).toBeFalsy();
+	});
 
-// 	it(`Must return Falsy`, () => {
-// 		expect(isValidEmail(emailWrong3)).toBeFalsy();
-// 	});
+	it(`Must return Falsy`, () => {
+		expect(isValidEmail(emailWrong3)).toBeFalsy();
+	});
 
-// 	it(`Must return Truthy`, () => {
-// 		expect(isValidEmail(emailCorrectly1)).toBeTruthy();
-// 	});
+	it(`Must return Truthy`, () => {
+		expect(isValidEmail(emailCorrectly1)).toBeTruthy();
+	});
 
-// 	it(`Must return Truthy`, () => {
-// 		expect(isValidEmail(emailCorrectly2)).toBeTruthy();
-// 	});
+	it(`Must return Truthy`, () => {
+		expect(isValidEmail(emailCorrectly2)).toBeTruthy();
+	});
 
-// 	it(`Must return Truthy`, () => {
-// 		expect(isValidEmail(emailCorrectly3)).toBeTruthy();
-// 	});
+	it(`Must return Truthy`, () => {
+		expect(isValidEmail(emailCorrectly3)).toBeTruthy();
+	});
 
-// 	// it(`Must return Truthy`, () => {
-// 	// 	expect(isValidEmail(undefined as unknown as string)).toEqual(false);
-// 	// });
-// });
+	it(`Must return Truthy`, () => {
+		expect(isValidEmail(undefined as unknown as string)).toEqual(false);
+	});
+});
